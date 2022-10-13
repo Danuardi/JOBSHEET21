f245592b41fd67c64f0cb742717946bbad0fe722
jobsheet6 <sunarniedisunandar@gmail.com>
2022-10-13 18:40:41 +0700

    JOBSHEET21

---
+++ app/src/main/java/com/example/job21/Kelas.kt
@@ -0,0 +1,5 @@
+package com.example.job21
+
+data class Kelas (
+    val nama: String?
+)
---
+++ app/src/main/java/com/example/job21/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.job21
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    val list = ArrayList<Kelas>()
+    val listKelas = arrayOf(
+        "ZULFA SOFIANA",
+        "AHMAD ARDHI SETIAWAN",
+        "ANISATUR ROFIQOH",
+        "AYU FEBRIANA PUTRI",
+        "BOWO SAKTI WIRATAMA",
+        "DANUARDI WAHYU PAMBUDI LAKSONO",
+        "DEVA BUDIANA",
+        "DWI FEBRI STIYANINGRUM",
+        "EVA ROHAS",
+        "FELIM EDI PRABOWO",
+        "FITRI RAHMAWATI"
+    )
+    lateinit var kelasView: RecyclerView
+    lateinit var kelasAdapter: KelasAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        kelasView = findViewById(R.id.kelasView)
+        kelasView.layoutManager = LinearLayoutManager(this)
+
+        for (i in listKelas.indices) {
+            list.add(Kelas(listKelas[i]))
+        }
+
+        kelasAdapter = KelasAdapter(list)
+        kelasAdapter.notifyDataSetChanged()
+        kelasView.adapter = kelasAdapter
+    }
+}
